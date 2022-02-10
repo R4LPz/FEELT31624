b27efa37fb96034ffd325ef14556eb3aae876727
Rafael Lucas <pereira.rafael.dev@gmail.com>
2022-02-09 21:00:54 -0300

    temporary changes

--- src/controllers/AirlineController.js
+++ src/controllers/AirlineController.js
@@ -6,20 +6,17 @@ class AirlineController{
         const {name, cnpj, airports} = req.body
         const airlineExists = await Airline.findOne({where: {cnpj: cnpj}})
 
-        //migrar para service
         if(!name || !cnpj || !airports){
             return res.status(400).send("Dados inválidos!")
         }
         if(airlineExists){
             return res.status(400).send("Airline já existe!")
         }
-        //validar se o cnpj é valido
         
         const newAirline = await Airline.create({
             name: name,
             cnpj: cnpj
         })
-        //migrar para service (add airports)
 
         if(airports.length > 0){
             for (let i = 0; i < airports.length; i++) {
@@ -27,21 +24,64 @@ class AirlineController{
             }
         }
 
-
-        
-        //newAirline.addAirport(airport)
-        
         return newAirline
         ? res.status(200).json(newAirline)
         : res.status(400).send("Falha ao criar a Airline")
     }
 
+    async update(req,res){
+        const id = req.params.id
+        const {name, cnpj, airports} = req.body
+
+        //validar campos
+        
+        const airlineUpdated = await Airline.update({
+            name: name,
+            cnpj: cnpj,
+        }, {where: {id:id}})
+        
+        const airline = await Airline.findByPk(id)
+        
+        if(airports.length > 0){
+            for (let i = 0; i < airports.length; i++) {
+                airline.addAirport(await Airport.findOne({where: {id: airports[i]}}))
+            }
+        }
+        return res.status(200).json(airlineUpdated)
+    }
+
+    async delete(req,res){
+        const id = req.params.id
+        
+        const data = await Airline.destroy({where: {id:id}})
+
+        return res.status(200).json(data)
+    }
+
+    async listByAirport(req,res){
+        const id = req.params.id
+        console.log(id)
+
+        const list = await Airline.findAll({
+            include: {
+                model: Airport, 
+                where: {
+                    id:id
+                }
+            }
+        })
+        return res.status(200).json(list)
+    }
+    
     async listById(req,res){
         const id = req.params.id
-        const airport = await Airport.findAll({where: {id: id}, include: Airline})
 
-        return res.status(200).json(airport)
-    }
+        const airline = await Airline.findByPk(id)
+
+        return !airline 
+        ? res.sstatus(400).send("Airline não encontrada") 
+        : res.status(200).json(airline)
+    }    
 }
 
 module.exports = AirlineController
--- src/routes.js
+++ src/routes.js
@@ -13,13 +13,46 @@ const airplane = new AirplaneController()
 
 const router = Router();
 
+//User Routes
 router.post("/user", user.create)
+//router.patch("/user/:id")
+//router.delete("/user/:id")
+//router.get("/user/listById")
+
+//Airport Routes
 router.post("/airport", airport.create)
-router.post("/airline", airline.create)
-router.post("/flight", flight.create)
+//router.patch("/airport/:id")
+//router.delete("/airport/:id")
+//router.get("/airport/:id")
+//router.get("/airport/listAirlines")
+//router.get("/airport/listFlights")
+//router.get("/airport/listByLocation")
+
+//Airplane Routes
 router.post("/airplane", airplane.create)
+//router.patch("/airplane/:id")
+//router.delete("/airplane/:id")
+//router.get("/airplane/:id")
+//router.get("/airplane/listByAirline")
+//router.get("/airplane/listByModel")
+//router.get("/airplane/listByBrand")
 
+//Flight Routes
+router.post("/flight", flight.create)
+//router.patch("/flight/:id")
+//router.delete("/flight/:id")
+//router.get("/flight/:id")
+//router.get("/flight/listByDate")
+//router.get("/flight/listByAirline")
+//router.get("/flight/listByAirport")
+//router.get("/flight/listByCapacity")
 
-router.get("/test/:id", flight.get)
+//Airlines Routes
+router.post("/airline", airline.create)
+router.patch("/airline/:id", airline.update)
+router.delete("/airline/:id", airline.delete)
+router.get('/airline/listByAirport/:id', airline.listByAirport)
+router.get('/airline/listById/:id', airline.listByAirport)
+//router.get('/airline/listByLocation/:id', airline.listByAirport)
 
 module.exports = router
